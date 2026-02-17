4de58d26ca8c0113a6a5c072ebb26a123d2564d5
abdulrahman-nisar <abdulrahmannisar6@gmail.com>
2026-02-17 23:06:32 +0500

    Implementing Receiving data code from main activity 1 in main activity 2

--- app/src/main/java/com/example/intents_in_android/MainActivity2.kt
+++ app/src/main/java/com/example/intents_in_android/MainActivity2.kt
@@ -1,6 +1,7 @@
 package com.example.intents_in_android
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,12 @@ class MainActivity2 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val name = intent.getStringExtra("name")
+        val age = intent.getIntExtra("age", 0)
+        val textViewName = findViewById<TextView>(R.id.name)
+        val textViewAge = findViewById<TextView>(R.id.age)
+        textViewName.text = name
+        textViewAge.text = age.toString()
     }
 }
